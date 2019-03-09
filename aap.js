e15fd356e433bcc533fab526cc732c4867923789
h22121 <h22121@lab.cpcc.edu>
2019-03-09 12:54:49 -0500

    WorkWithData20190309

---
+++ aap.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let myName = "Daneen Carroll";
+
+let myNumber = 10;
+myNumber = 10e-1;
+myNumber = 0x00F;
+myNumber = 0o007;
+myNumber = Infinity;
+
+let myFirstName = "Daneen";
+let myLastName = "Carroll";
+let myFullName = 'First Name: \t${myFirstName}\nlast Name: \t${myLastName}';
+let a = 8;
+let valueA = 10;
+let valueB = "5" * a;
+
+let total = valueA * valueB;
+
+let fullName = myFirstName + " " + myLastName;
+
+console.log(myName);
